94a501267a6b90ea97b4bb95aa9794bb8234622e
coxine <59347549+coxine@users.noreply.github.com>
2024-05-25 23:23:05 +0800

    +title favicon

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,6 +4,8 @@ import { createTheme, ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import App from '@/pages/App';
 import { green, lightGreen } from '@mui/material/colors';
+import Head from 'next/head';
+
 
 
 function Index() {
@@ -22,10 +24,20 @@ function Index() {
     );
 
     return (
-        <ThemeProvider theme={theme}>
-            <CssBaseline />
-            <App />
-        </ThemeProvider>
+        <div>
+            <Head>
+                <title>图神庙</title>
+                <meta name="keywords" content="图神庙,txt2img,文本生成图片,生成图片" />
+                <meta property="og:title" content="图神庙" />
+                <meta property="og:description" content="一个生成图片的网站，输入文字，生成图片" />
+                <meta property="og:image" content="/favicon.svg" />
+                <link rel="icon" href="/favicon.svg" />
+            </Head>
+            <ThemeProvider theme={theme}>
+                <CssBaseline />
+                <App />
+            </ThemeProvider>
+        </div>
     );
 }
 
